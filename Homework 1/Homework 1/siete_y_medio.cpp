4a6dddd2bc64c6850fdf500f54b152e79045bbf7
Allison Cosca-Baresh <amcoscabaresh@gmail.com>
2017-10-15 15:31:14 -0700

    Created member functions to operate player's turn and dealer's turn

--- Homework 1/Homework 1/siete_y_medio.cpp
+++ Homework 1/Homework 1/siete_y_medio.cpp
@@ -8,13 +8,62 @@
 using namespace std;
 
 
-// Non member functions declarations (if any)
+//Allows player to continue to draw cards until they decide to stop
+void player_turn(int bet, Player person, Hand p);
+
+void dealer_turn(Player dealer, Hand d);
+
+//Allows player to continue to draw cards until they decide to stop
+void player_turn(int bet, Player person, Hand p) {
+	char keep_playing = 'y';
+
+	//Requests bet from player, requires that bet not be greater than the money the player currently has
+	cout << "You have $" << person.get_money() << ". Enter bet: ";
+	cin >> bet;
+	while (bet > person.get_money()) {
+		cout << "You cannot bet more money than you currently have." << endl << "Enter bet: ";
+		cin >> bet;
+	}
+	p.clear_hand();
+
+	//Continues to draw and display cards as long as player continues to request more cards
+	while (keep_playing == 'y') {
+		Card player_card = Card();
+		p.draw(player_card);
+
+		cout << "New card:" << endl << '\t';
+		cout << player_card.get_spanish_rank() << " de " << player_card.get_spanish_suit();
+		cout << " (" << player_card.get_english_rank() << " of " << player_card.get_english_suit() << ")." << endl << endl;
+
+		cout << "Your cards:" << endl;
+		p.display();
 
 
-// Non member functions implementations (if any)
+		cout << "Your total is " << p.get_sum() << ". Do you want another card (y/n)? ";
+		cin >> keep_playing;
+	}
+}
 
+void dealer_turn(Player dealer, Hand d) {
+	d.clear_hand();
+	cout << "Dealer's cards: " << endl;
+	double dealer_total = d.get_sum();
 
-// Stub for main
+	while (dealer_total < 5.5) {
+		Card dealer_card = Card();
+		d.draw(dealer_card);
+
+		cout << "New card:" << endl << '\t';
+		cout << dealer_card.get_spanish_rank() << " de " << dealer_card.get_spanish_suit();
+		cout << " (" << dealer_card.get_english_rank() << " of " << dealer_card.get_english_suit() << ")." << endl << endl;
+
+		cout << "Dealer's cards:" << endl;
+		d.display();
+		cout << "The dealer's total is " << d.get_sum() << "." << endl << endl;
+		dealer_total = d.get_sum();
+	}
+}
+
 int main() {
 	int bet;
 	Player person = Player(100);
@@ -23,49 +72,10 @@ int main() {
 	Hand d = dealer.get_hand();
 
 	while (person.get_money() != 0 && dealer.get_money() > 0) {
-		char keep_playing = 'y';
 
-		cout << "You have $" << person.get_money() << ". Enter bet: ";
-		cin >> bet;
-		while (bet > person.get_money()) {
-			cout << "You cannot bet more money than you currently have." << endl << "Enter bet: ";
-			cin >> bet;
-		}
-		p.clear_hand();
+		player_turn(bet, person, p);
 
-		while (keep_playing == 'y') {
-			Card player_card = Card();
-			p.draw(player_card);
-
-			cout << "New card:" << endl << '\t';
-			cout << player_card.get_spanish_rank() << " de " << player_card.get_spanish_suit();
-			cout << " (" << player_card.get_english_rank() << " of " << player_card.get_english_suit() << ")." << endl << endl;
-
-			cout << "Your cards:" << endl;
-			p.display();
-
-
-			cout << "Your total is " << p.get_sum() << ". Do you want another card (y/n)? ";
-			cin >> keep_playing;
-		}
-
-		d.clear_hand();
-		cout << "Dealer's cards: " << endl;
-		double dealer_total = d.get_sum();
-
-		while (dealer_total < 5.5) {
-			Card dealer_card = Card();
-			d.draw(dealer_card);
-
-			cout << "New card:" << endl << '\t';
-			cout << dealer_card.get_spanish_rank() << " de " << dealer_card.get_spanish_suit();
-			cout << " (" << dealer_card.get_english_rank() << " of " << dealer_card.get_english_suit() << ")." << endl << endl;
-
-			cout << "Dealer's cards:" << endl;
-			d.display();
-			cout << "The dealer's total is " << d.get_sum() << "." << endl << endl;
-			dealer_total = d.get_sum();
-		}
+		dealer_turn(dealer, d);
 
 		double player_total = p.get_sum();
 		bool player_win = false;
